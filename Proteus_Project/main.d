5218bdaac7abf6caa4689dfc72aef89a71bd3a33
23stummala <tummalasiddarth@gmail.com>
2024-04-15 17:07:46 -0400

    Current code

---
+++ Proteus_Project/main.d
@@ -0,0 +1,21 @@
+../main.d: ../main.cpp Libraries/FEHIO.h Startup/derivative.h \
+ Startup/MK60DZ10.h Drivers/adc16.h Libraries/FEHLCD.h Drivers/spi.h \
+ ../fehproteusfirmware/Startup/derivative.h Libraries/FEHUtility.h \
+ Libraries/LCDColors.h Libraries/FEHMotor.h Libraries/FEHServo.h \
+ Libraries/FEHBattery.h Libraries/FEHIO.h Libraries/FEHRCS.h \
+ Drivers/FEHXBee.h
+Libraries/FEHIO.h:
+Startup/derivative.h:
+Startup/MK60DZ10.h:
+Drivers/adc16.h:
+Libraries/FEHLCD.h:
+Drivers/spi.h:
+../fehproteusfirmware/Startup/derivative.h:
+Libraries/FEHUtility.h:
+Libraries/LCDColors.h:
+Libraries/FEHMotor.h:
+Libraries/FEHServo.h:
+Libraries/FEHBattery.h:
+Libraries/FEHIO.h:
+Libraries/FEHRCS.h:
+Drivers/FEHXBee.h:
